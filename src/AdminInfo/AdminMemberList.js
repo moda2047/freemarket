0a1ed154914e7ab50267b9e8f870e63b270b1793
moda <moda2047@naver.com>
2023-11-09 17:24:33 +0900

    회원 목록 조회 페이징까지 구현(아직 검색남음)

--- src/AdminInfo/AdminMemberList.js
+++ src/AdminInfo/AdminMemberList.js
@@ -1,350 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./AdminMemberList.css";
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
 function AdminMemberList() {
   const [members, setMembers] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [indexOfFirstMember, setIndexOfFirstMember] = useState(0);
-  const membersPerPage = 10;
+  const [cookies] = useCookies(["token"]);
 
-  // Mock data
-  const mockData = [
-    // Sample member data
-    {
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@example.com",
-      phoneNumber: "555-123-4567",
-    },
-    {
-      id: 2,
-      name: "Jane Smith",
-      email: "jane.smith@example.com",
-      phoneNumber: "555-987-6543",
-    },
-    {
-      id: 4,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 3,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 5,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 6,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 8,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 79,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 70,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-    {
-      id: 7,
-      name: "전승기",
-      email: "bob.johnson@example.com",
-      phoneNumber: "555-567-8901",
-    },
-  ];
-
-  const filteredMembers = mockData.filter(
-    (member) =>
-      member.id.toString().includes(searchQuery) ||
-      member.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  const indexOfLastMember = currentPage * membersPerPage;
-  const currentMembers = filteredMembers.slice(
-    indexOfFirstMember,
-    indexOfLastMember
-  );
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-  const handlePageChange = (pageNumber) => {
-    // 업데이트된 페이지 번호를 기반으로 indexOfFirstMember를 계산
-    const newIndexOfFirstMember = (pageNumber - 1) * membersPerPage;
-    setCurrentPage(pageNumber);
-
-    // indexOfFirstMember 업데이트
-    setIndexOfFirstMember(newIndexOfFirstMember);
+  const fetchData = async () => {
+    const mailAuthAPI = "http://localhost:8000/member/search";
+    const headers = {
+      headers: {
+        Authorization: cookies.token,
+        ContentType: "application/json",
+        Accept: "application/json",
+      },
+    };
+    console.log("fetchData 함수가 실행됩니다.");
+    console.log(typeof cookies.token);
+    axios
+      .get(mailAuthAPI, headers)
+      .then((response) => {
+        console.log("API 응답 데이터: ", response.data);
+        if (response.data.result) {
+          const foundArray = response.data.found;
+          setMembers(foundArray);
+          setTotalCount(response.data.totalCount);
+          console.log("성공적으로 됐");
+        } else {
+          console.log("실패");
+        }
+      })
+      .catch((error) => {
+        console.error("오류 : ", error);
+        console.error("Error status: ", error.response.status);
+        console.error("Error data: ", error.response.data);
+      });
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const [totalCount, setTotalCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  useEffect(() => {
+    // Ensure currentPage is within a valid range
+    if (currentPage < 1) {
+      setCurrentPage(1);
+    }
+    if (currentPage > Math.ceil(totalCount / itemsPerPage)) {
+      setCurrentPage(Math.ceil(totalCount / itemsPerPage));
+    }
+  }, [currentPage, totalCount]);
+
+  const prevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
   const handleSearch = () => {};
 
   const handleSanction = () => {};
@@ -375,22 +92,22 @@ function AdminMemberList() {
               className="AdminMemberListSearchInput"
               type="text"
               placeholder="ID 또는 이름으로 검색"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
+              //value={searchQuery}
+              //onChange={(e) => setSearchQuery(e.target.value)}
               style={{
                 float: "right",
                 width: "150px",
-              }} /* 오른쪽 정렬 및 크기 조정 */
-              maxLength="15" /* 최대 15글자로 제한 */
+              }}
+              maxLength="15"
             />
           </div>
 
           <table className="AdminMListSearchTable">
             <colgroup>
-              <col width="10%" />
-              <col width="10%" />
+              <col width="15%" />
+              <col width="15%" />
               <col width="30%" />
-              <col width="30%" />
+              <col width="25%" />
               <col width="auto" />
             </colgroup>
             <tr>
@@ -401,29 +118,38 @@ function AdminMemberList() {
               <th>Actions</th>
             </tr>
 
-            {currentMembers.map((member) => (
-              <tr key={member.id}>
-                <td>{member.id}</td>
-                <td>{member.name}</td>
-                <td>{member.email}</td>
-                <td>{member.phoneNumber}</td>
-                <td>
-                  <button onClick={() => handleSanction(member.id)}>
-                    제재
-                  </button>
-                </td>
-              </tr>
-            ))}
+            {members
+              .slice(
+                (currentPage - 1) * itemsPerPage,
+                currentPage * itemsPerPage
+              )
+              .map((member) => (
+                <tr key={member.id}>
+                  <td>{member.id}</td>
+                  <td>{member.name}</td>
+                  <td>{member.email}</td>
+                  <td>{member.mobile}</td>
+                  <td>
+                    <button onClick={() => handleSanction(member.id)}>
+                      제재
+                    </button>
+                  </td>
+                </tr>
+              ))}
           </table>
-          <div className="AdminMemberListpagination">
-            {Array.from({
-              length: Math.ceil(filteredMembers.length / membersPerPage),
-            }).map((_, index) => (
-              <button key={index} onClick={() => handlePageChange(index + 1)}>
-                {index + 1}
-              </button>
-            ))}
+          <div className="MyBuyListPagination">
+            <button onClick={prevPage} disabled={currentPage === 1}>
+              이전
+            </button>
+            <span>{currentPage}</span>
+            <button
+              onClick={nextPage}
+              disabled={currentPage >= Math.ceil(totalCount / itemsPerPage)}
+            >
+              다음
+            </button>
           </div>
+          <br></br>
         </div>
       </div>
     </div>
