3d31a79b6f0b9a755e0f45bd2be775f9eccaac4e
moda <moda2047@naver.com>
2023-11-15 22:28:19 +0900

    moment.js 추가 및 제재 기능 완성

--- src/AdminInfo/AdminMemberRestriction.js
+++ src/AdminInfo/AdminMemberRestriction.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "./AdminMemberRestriction.css";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import moment from "moment";
 
 const AdminMemberRestriction = () => {
   const [reason, setReason] = useState("");
@@ -8,6 +11,7 @@ const AdminMemberRestriction = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { memberId } = location.state;
+  const [cookies] = useCookies(["token"]);
   const handleReasonChange = (e) => {
     setReason(e.target.value);
   };
@@ -16,14 +20,79 @@ const AdminMemberRestriction = () => {
     setDuration(e.target.value);
   };
 
-  const handleSubmit = () => {
-    // memberId, reason, duration을 서버로 전송하거나 저장할 수 있습니다.
-    console.log("회원 ID:", memberId);
-    console.log("제재 사유:", reason);
-    console.log("제재 기간:", duration);
+  const calculateEndDate = () => {
+    let endDate;
+    switch (duration) {
+      case "1day":
+        endDate = moment().add(1, "days");
+        break;
+      case "3days":
+        endDate = moment().add(3, "days");
+        break;
+      case "1week":
+        endDate = moment().add(1, "weeks");
+        break;
+      case "1month":
+        endDate = moment().add(1, "months");
+        break;
+      case "1year":
+        endDate = moment().add(1, "years");
+        break;
+      case "permanent":
+        endDate = moment("9999-12-31 00:00:00", "YYYY-MM-DD HH:mm:ss");
+        break;
+      default:
+        endDate = moment().add(1, "days");
+    }
+    return endDate === "permanent"
+      ? "permanent"
+      : endDate.format("YYYY-MM-DD HH:mm:ss");
   };
 
-  const handleCancelClick = () => {};
+  const handleSubmit = () => {
+    if (!reason.trim()) {
+      window.alert("제재 이유을 입력하세요.");
+      return;
+    }
+    const endDate = calculateEndDate();
+    const data = {
+      id: memberId,
+      reason: reason,
+      expire_at:
+        endDate === "permanent" ? "permanent" : moment(endDate).toISOString(),
+    };
+    const url = "http://localhost:8000/sanction/impose";
+    const headers = {
+      headers: {
+        Authorization: cookies.token,
+        ContentType: "application/json",
+        Accept: "application/json",
+      },
+    };
+
+    axios
+      .post(url, data, headers)
+
+      .then((response) => {
+        console.log(response);
+        if (response.data.result) {
+          window.alert("제재가 성공적으로 마무리 되었습니다.");
+          console.log(response.data.message);
+          navigate("../AdminMain");
+        } else {
+          console.log(response.data.message);
+          window.alert(response.data.message);
+        }
+      })
+      .catch((error) => {
+        window.alert("회원 제재 중 오류가 발생했습니다.");
+        console.error("회원 제재 중 오류가 발생했습니다.", error);
+      });
+  };
+
+  const handleCancelClick = () => {
+    navigate("../AdminMain");
+  };
 
   return (
     <div className="AdminMemberRestrictionContainer">
